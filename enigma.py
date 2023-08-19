5bf52219926b6d5a3e8c731f99f49387a55d286b
M4PERFECT <jagdeepbhani@gmail.com>
2023-08-18 23:44:47 -0400

    did a lot, now fixing the rotors rotation logics

--- enigma.py
+++ enigma.py
@@ -1,155 +1,226 @@
 
 
 import random
-from typing import Self
+import json
+# need to fix the position independence of the rotor's encryption/decryption
+
+def generate_wiring(num_pairs=10):
+    wiring = {}
+    alphabet = list('abcdefghijklmnopqrstuvwxyz')
+    keys = random.sample(alphabet, k=num_pairs)
+    values_list = [letter for letter in alphabet if letter not in keys]
+    values = random.sample(values_list, k=num_pairs)
+    for key, value in zip(keys, values):
+        wiring[key] = value
+        wiring[value] = key
+    return wiring
 
 
-class Rotor: 
+class Rotor:
     def __init__(self):
-        self.position = self.setPosition()
-        self.offset = self.setOffset()
-        self.wiring = self.setWiring()
-        self.rotation = self.setRotation()
-        print(f"Rotor-position: {self.position}\n Rotor-offset: {self.offset}\n rotation: {self.rotation}\n Wiring: {''.join(self.wiring)}")
+        self.name = str()
+        self.signal = list()
+        self.position = self.set_position()
+        self.offset = self.set_offset()
+        self.wiring = self.generate_wiring()
+        self.rotation = self.set_rotation()
         
+    def get_rotor(self):
+        return {
+        "Rotor-position": self.position,
+        "Rotor-offset": self.offset,
+        "rotation": self.rotation,
+        "Wiring": ''.join(self.wiring)
+        }
+    
     def turn(self):
-            self.rotation += 1
-            if self.rotation == 27:
-                self.rotation = 0    
-            return self.rotation            
-    def encrypt(self, letter):
-            wiring = self.wiring
-            orignalAlphabeticalOrder = ord(letter) - 96
-            newLetter = wiring[orignalAlphabeticalOrder - 1]
-            return newLetter
-    def decrypt(self, letter):
-            wiring = self.wiring
-            encryptedAlphabeticalOrder = ord(letter) - 96
-            orignalLetter = wiring[encryptedAlphabeticalOrder -1]
-            return orignalLetter 
+        self.rotation += 1
+        if self.rotation == 27:
+            self.rotation = 0
+        self.wiring.insert(0, self.wiring.pop())
+        return self.rotation
+    
+    def encrypt(self, signal):
+        wiring = self.wiring
+        signal_alphabetical_order = ord(signal) - 96
+        new_signal = wiring[signal_alphabetical_order - 1]
+        return new_signal
 
-        
+    def decrypt(self, signal):
+        wiring = self.wiring
+        signal_alphabetical_order = ord(signal) - 96
+        new_signal = wiring[signal_alphabetical_order - 1]
+        return new_signal
+
     @staticmethod
-    def setPosition():
-            print("Please enter the position of the rotor. (1-26)")
-            position = int(input())
-            return position
-    
+    def set_position():
+        position = 1
+        return position
+
     @staticmethod
-    def setOffset():
-            print("Would you like set an offset? (Y/N)")
-            response = input().lower() 
-            if response == "y":
-                print("Please enter the value of the rotor offset.")
-                offset = int(input())
-            else :
-                print("setting a default offset (1 or 2)")
-                offset = random.randint(1,2)
-            return offset
-    
+    def set_offset():
+        offset = random.randint(1, 2)
+        return offset
+
     @staticmethod
-    def setWiring():
-            print("Would you like to input a wiring? (Y/N)")
-            response = input().lower()
-            if response != "y":
-                wiring = []
-                i_list = list("abcdefghijklmnopqrstuvwxyz")
-                while len(i_list) > 0:
-                        patternNumber = random.randint(0, (len(i_list)-1))
-                        wiring.append(i_list[patternNumber])
-                        i_list.pop(patternNumber)
-            else:
-                print("Please enter the wiring list. only 'a' format input." )
-                wiring = list(input())
-            return wiring
-    
+    def generate_wiring():
+        wiring = []
+        i_list = list("abcdefghijklmnopqrstuvwxyz")
+        while len(i_list) > 0:
+            pattern_number = random.randint(0, (len(i_list)-1))
+            wiring.append(i_list[pattern_number])
+            i_list.pop(pattern_number)    
+        return wiring
+
     @staticmethod
-    def setRotation():
-        rotation = random.randint(1,26)
+    def set_rotation():
+        rotation = random.randint(1, 26)
         return rotation
-    
+
 
 class Plugboard:
     def __init__(self):
-        self.wiring = self.setWiring()
-        
-    def swap_with_plugboard(letter, plugboard):
-        return plugboard.get(letter, letter)
+        self.signal = self.set_signal()
+        self.wiring = self.generate_wiring()
+
+    def get_plugboard(self):
+        return self.wiring
+    
+    def swap_with_plugboard(self, signal):
+        return self.wiring.get(signal, signal)
 
     @staticmethod
-    def setWiring():
-        wiring = {}
-        alphabet = ["abcdefghijklmnopqrstuvwxyz"]
-        keys = random.choices(alphabet, 10)
-        values = alphabet.remove(keys)
-        for i in keys:
-            wiring.update(keys[i], values[i])
-            values.remove(values[i])
-        wiring.update(values)  
-        print(wiring)  
+    def generate_wiring():
+        wiring = generate_wiring(num_pairs=10)
         return wiring
+    
+    def set_wiring(self, wiring):
+        self.wiring = wiring
+        return 
+
+    @staticmethod
+    def set_signal():
+        return list()
+
 
 class Reflector:
-   def __init__(self):
-    self.signal = []
+    def __init__(self):
+        self.signal = self.set_signal()
+        self.letter_mapping = self.generate_letter_mapping()
+
+    def get_reflector(self):
+        return self.letter_mapping
+    
+    def set_letter_mapping(self, letter_mapping):
+        self.letter_mapping = letter_mapping
+        return
+    
+    def reflect_signal(self, signal):
+        return self.letter_mapping.get(signal, signal)
+    
+    @staticmethod
+    def set_signal():
+        signal = list()
+        return signal
+
+    @staticmethod
+    def generate_letter_mapping():
+        letter_mapping = generate_wiring(num_pairs=13)
+        return letter_mapping
+
 
 class EnigmaMachine:
+    def __init__(self, num_rotors):
+        self.plugboard = Plugboard()
+        self.rotors = []
+        for i in range(1, num_rotors + 1):
+            rotor = Rotor()
+            rotor.name = "rotor" + str(i)
+            self.rotors.append(rotor)
+            print(f"rotor created. {rotor.name} ")
+        self.reflector = Reflector()
+
+    def get_engima(self):
+        enigma_config = {
+        'plugboard': self.plugboard.get_plugboard(),
+        'rotors': [rotor.get_rotor() for rotor in self.rotors],
+        'reflector': self.reflector.get_reflector()
+        }
+        return json.dumps(enigma_config, indent=4)
+
+    def encrypt(self, signal):
+        signal = self.plugboard.swap_with_plugboard(signal)
+        for rotor in self.rotors:
+            signal = rotor.encrypt(signal)
+            rotor.turn()
+        signal = self.reflector.reflect_signal(signal)
+        self.rotors.reverse()
+        for rotor in self.rotors:
+            signal = rotor.encrypt(signal)
+        signal = self.plugboard.swap_with_plugboard(signal)
+        return signal
     
-    plugboard = Plugboard()
-    reflector = Reflector()
+    def decrypt(self, signal):
+        signal = self.plugboard.swap_with_plugboard(signal)
+        for rotor in self.rotors:
+            signal = rotor.decrypt(signal)
+            rotor.turn()
+            print(f"Rotor rotated. {rotor.position}")
+        signal = self.reflector.reflect_signal(signal)
+        self.rotors.reverse()
+        for rotor in self.rotors:
+            signal = rotor.decrypt(signal)
+        signal = self.plugboard.swap_with_plugboard(signal)
+        return signal
 
-    def start(self, letter):
-        self.rotor1.encrypt(letter)
-        return letter
-    def __init__(self):
-        print("Please enter the first rotor's rotation.")
-        # rotorRotation = int(input())
-        # rotorWiring = list(input())
-        self.rotor2 = Rotor()
-        self.rotor3 = Rotor()
-        self.rotor1 = Rotor()
 
-        pass
 def main():
- while(True):
-    print("-------------------------")
-    print("Enigma online. Please enter a option")
-    print("1) Encrpty a letter.")
-    print("2) Decrypt a leter.")
-    print("3) Encrypt a passage.")
-    print("4) Decrypt a passage.")
-    print("5) Exit")
-    option = int(input())
+    while (True):
+        print("------------------------------------")
+        print("Enigma online. Please enter a option")
+        print("1) Encrpty a signal.")
+        print("2) Decrypt a signal.")
+        print("3) Exit.")
+        print("------------------------------------")
 
-    match option:
-     case 1:
-          rotor1 = Rotor()
-          print("Please input a letter to encrypt:")
-          letter = input()
-          print(f"The encrypted letter for {letter} is {rotor1.encrypt(letter)}")
-     case 2:
-          rotor1 = Rotor()
-          print("Please input the letter to decrypt:")
-          encryptedLetter = input()
-          print(f"The orignal letter for {encryptedLetter} is {rotor1.decrypt(encryptedLetter)}")
-     case 3:
-          return 0
-     case 4:
-          return 0
-     case 5:
-          exit()
+        option = int(input())
 
+        match option:
+            case 1:
+                enigma_3 = EnigmaMachine(num_rotors=3)
+                with open('enigma_config.json', 'w') as file:
+                    file.write(enigma_3.get_engima())
+                print("Please input a signal to encrypt:")
+                signals = input().lower()
+                encrypted_signals = ''
+                for signal in signals:
+                    encrypted_signals += enigma_3.encrypt(signal)
+                print(
+                    f"The encrypted signal for {signals} is {encrypted_signals}")
+            case 2:
+                with open('enigma_config.json', 'r') as file:
+                    config = json.load(file)
+                wiring = config['plugboard']
+                enigma_3.plugboard.set_wiring(wiring)
+                rotor_configs = config['rotors']
+                for i, rotor_config in enumerate(rotor_configs):
+                    enigma_3.rotors[i].position = rotor_config["Rotor-position"]
+                    enigma_3.rotors[i].offset = rotor_config["Rotor-offset"]
+                    enigma_3.rotors[i].rotation = rotor_config["rotation"]
+                    enigma_3.rotors[i].wiring = list(rotor_config["Wiring"])
+                letter_mapping = config['reflector']
+                enigma_3.reflector.letter_mapping = letter_mapping
 
-
-
-
-
-
-
-
-
+                print("Please input a signal to decrypt:")
+                signals = input().lower()
+                decrypted_signals = ''
+                for signal in signals:
+                    decrypted_signals += enigma_3.decrypt(signal)
+                print(
+                    f"The encrypted signal for {signals} is {decrypted_signals}")
+            case 3:
+                exit()
 
 
 if __name__ == "__main__":
     main()
-    
